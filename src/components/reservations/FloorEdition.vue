68220f8d2a6a4f4bb1b2a138333f10dfc423a6ca
Rafał Chełkowski <rchelek@gmail.com>
2020-03-15 15:11:38 +0100

    Utworzenie podstawowych komponentów

---
+++ src/components/reservations/FloorEdition.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-row dense justify="center">
+    <v-col dense cols="5">
+      <v-form
+        ref="form"
+        class="d-flex justify space-between flex-column flex-md-row"
+        v-model="valid"
+      >
+        <v-col
+          v-for="(textField, index) in textFields"
+          dense
+          md="4"
+          class="py-0"
+          :key="index"
+        >
+          <v-text-field
+            solo
+            dense
+            color="black"
+            :flat="!edit"
+            :prefix="textField.prefix"
+            :placeholder="textField.placeholder"
+            :readonly="!edit"
+            :rules="[rules.required, rules.number]"
+          />
+        </v-col>
+        <v-tooltip right>
+          <template v-slot:activator="{ on }">
+            <v-btn v-on="on" @click="toggleEdit()" icon>
+              <v-icon>mdi-pencil</v-icon>
+            </v-btn>
+          </template>
+          <span> Edytuj </span>
+        </v-tooltip>
+
+      </v-form>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    edit: false,
+    valid:false,
+    textFields: [
+      {
+        prefix: "Piętro",
+        placeholder: "nr"
+      },
+      {
+        prefix: "Od",
+        placeholder: "nr Pokoju"
+      },
+      {
+        prefix: "Do",
+        placeholder: "nr Pokoju"
+      }
+    ],
+    rules: {
+      required: v => !!v || "Wymagane",
+      number: v => !isNaN(v) || "Podaj liczbę"
+    }
+  }),
+  methods: {
+    toggleEdit() {
+        console.log(!this.edit, this.valid)
+        if(!this.edit || this.valid){
+            
+            this.edit = !this.edit
+        }
+    }
+  }
+};
+</script>
+
+<style></style>
---
+++ src/components/reservations/RoomReservation.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-card class="mx-auto" flat outlined >
+    <v-toolbar short>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn v-on="on" icon>
+            <v-icon>mdi-arrow-left</v-icon>
+          </v-btn>
+        </template>
+        <span>Wróć</span>
+      </v-tooltip>
+      <v-toolbar-title>
+        Piętra/numery pokojów
+      </v-toolbar-title>
+      <v-spacer />
+      <v-btn icon>
+        <v-icon>
+          mdi-bell-outline
+        </v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-card-text>
+      <v-row justify="center" dense>
+        <v-col cols="5">
+          <p class="title text-center">
+            Wpisz numery pięter i zakres numeracji pokojów/apartementów, które
+            na nich występują
+          </p>
+          <p class="text-center">
+            <v-icon>mdi-lightbulb-on-outline</v-icon>
+            Aby edytować zakres, lub wpisać poszczególne numery ręcznie, kliknij
+            ikonę ołówka
+          </p>
+        </v-col>
+      </v-row>
+      <v-container>
+        <floor-edition/>
+        <v-row dense justify="center">
+          <v-btn color="primary">
+            Dodaj piętro
+            <v-icon right>mdi-plus-circle</v-icon>
+          </v-btn>
+        </v-row>
+      </v-container>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer />
+      <v-btn color="success" > Dalej </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import FloorEdition from './FloorEdition'
+export default {
+  components:{
+    'floor-edition': FloorEdition
+  },
+  mounted() {}
+};
+</script>
+
+<style></style>
